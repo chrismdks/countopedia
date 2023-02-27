e6049bc686bf9d28aa0d339a86e1b26c706a52f9
chrismdks <chris.mdks@gmail.com>
2023-02-27 16:26:05 +0200

    initialize & retrieve state

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 class Counter extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            count:0
+        }
+    }
+
     handleAttack(){
         alert("Attack clicked");
     }
@@ -12,7 +19,7 @@ class Counter extends React.Component{
     render(){
         return(
             <div className='row text-white'>
-                <h2>Counter:</h2>
+                <h2>Counter: {this.state.count}</h2>
                     <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
                     <button onClick={this.handleDefense} style={{width:"200px"}}>-1</button>
             </div>
